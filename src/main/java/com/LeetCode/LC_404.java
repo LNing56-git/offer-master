bc61de46cb366d336cba4ee1a5b98af19a275afb
LNing56-git <ln1447615172@163.com>
2019-12-16 16:10:38 +0800

    左叶子之和

--- src/main/java/com/LeetCode/LC_404.java
+++ src/main/java/com/LeetCode/LC_404.java
@@ -1,4 +1,16 @@
 package com.LeetCode;
 
+/**
+ * 计算给定二叉树的所有左叶子之和
+ */
 public class LC_404 {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root==null)return 0;
+        return sum(root.left)+sumOfLeftLeaves(root.right);
+    }
+    public int sum(TreeNode treeNode){
+        if(treeNode==null)return 0;
+        if(treeNode.left==null&&treeNode.right==null)return treeNode.val;
+        return sum(treeNode.left)+sumOfLeftLeaves(treeNode.right);
+    }
 }
