5a4ac24ad214b6ea00865a98992f87a114fb18bf
LNing56-git <ln1447615172@163.com>
2019-12-24 17:11:15 +0800

    bst剪枝 保留[L,R]区间的值

---
+++ src/main/java/com/LeetCode/LC_669.java
@@ -0,0 +1,15 @@
+package com.LeetCode;
+
+public class LC_669 {
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if(root==null)return null;
+        if(root.val>R)return trimBST(root.left,L,R);
+        if(root.val<L)return trimBST(root.right,L,R);
+        root.left = trimBST(root.left,L,R);
+        root.right = trimBST(root.right,L,R);
+        return root;
+    }
+    public static void main(String[] args) {
+
+    }
+}
