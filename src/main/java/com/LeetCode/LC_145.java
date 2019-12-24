e1d83d72d9e7cbe7bf3ce99fc78e13f0f0335fa4
LNing56-git <ln1447615172@163.com>
2019-12-24 20:58:00 +0800

    非递归后序二叉树

---
+++ src/main/java/com/LeetCode/LC_145.java
@@ -0,0 +1,26 @@
+package com.LeetCode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 非递归后序遍历二叉树
+ */
+public class LC_145 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> re = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        if(root==null)return re;
+        stack.push(root);
+        while (stack.size()!=0){
+          TreeNode treeNode = stack.pop();
+          re.add(treeNode.val);
+          if(treeNode.left!=null)stack.push(treeNode.left);
+          if(treeNode.right!=null)stack.push(treeNode.right);
+        }
+        Collections.reverse(re);
+        return re;
+    }
+}
