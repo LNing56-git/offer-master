6ab4aba3b16cc1d94baecfb16e4bf470a03c871e
LNing56-git <ln1447615172@163.com>
2020-01-19 16:38:26 +0800

    排序

---
+++ src/main/java/com/LeetCode/LC_1.java
@@ -0,0 +1,20 @@
+package com.LeetCode;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+
+public class LC_1 {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(nums[0],0);
+        for(int i = 1;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                return new int[]{map.get(target-nums[i]),i};
+            }else {
+                map.put(nums[i],i);
+            }
+        }
+        return null;
+    }
+}
